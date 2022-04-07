7a1f91f7a0209216d749df0792422b84e58c46ba
nizihad824 <mdnurulislam97@gmail.com>
2022-04-07 22:06:39 +0200

    posts data load

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Posts = () => {
+    const [posts,setPosts]=useState([]);
+    useEffect(()=>{
+        const url=`https://jsonplaceholder.typicode.com/posts`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setPosts(data))
+    },[])
+    return (
+    
+        <div>
+             <h2>Every posts Facebook ever had:{posts.length} </h2>
+
+             {
+                 posts.map(post =><Link
+                 key={post.id}
+                 to={`/posts/${post.id}`}
+
+                 > {post.id} </Link>)
+             }
+             <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Posts;
